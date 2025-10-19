46773c63ba3ac56df68173a52fd35ea0b3394c3d
Võ Duy Toàn <voduytoansgu@gmail.com>
2025-10-19 20:27:48 +0700

    Update socketService.js

--- client/src/services/socketService.js
+++ client/src/services/socketService.js
@@ -11,7 +11,7 @@ class SocketService {
       return this.socket
     }
 
-    this.socket = io(import.meta.env.VITE_SOCKET_URL || 'http://localhost:4000', {
+    this.socket = io(import.meta.env.VITE_SOCKET_URL || 'http://localhost:3007', {
       auth: {
         token
       },
