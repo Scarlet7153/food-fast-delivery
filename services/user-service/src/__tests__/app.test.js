bede8c5207043e9b22f091b087051f87a0fc0279
Võ Duy Toàn <voduytoansgu@gmail.com>
2025-11-15 20:42:54 +0700

    test

--- services/user-service/src/__tests__/app.test.js
+++ services/user-service/src/__tests__/app.test.js
@@ -57,5 +57,11 @@ describe('User Service App', () => {
       expect(response.body).toHaveProperty('path');
     });
   });
+
+  describe("fail", () => {
+    it("test fail", () => {
+      expect(1).toBe(2);
+    });
+  });
 });
 
