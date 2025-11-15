28d175144254083bda627583ad4781c9ef74b68e
Võ Duy Toàn <voduytoansgu@gmail.com>
2025-11-15 20:33:08 +0700

    test fail

--- services/user-service/src/app.js
+++ services/user-service/src/app.js
@@ -6,6 +6,7 @@ const config = require('./config/env');
 const database = require('./config/database');
 const logger = require('./utils/logger');
 const { register, metricsMiddleware } = require('./utils/metrics');
+const abc = ;
 
 // Import routes
 const userRoutes = require('./routes/user.routes');
